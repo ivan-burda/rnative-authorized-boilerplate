ba66c2584763c821baa35e06a8fb02170e03f947
ivanekBurda <burda.ivan@gmail.com>
2024-05-19 12:08:38 +0200

    reorg props

--- components/IconButton/IconButton.tsx
+++ components/IconButton/IconButton.tsx
@@ -1,25 +1,25 @@
 import {FC} from "react";
 import {Pressable, View, Text, StyleSheet} from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import {Ionicons} from '@expo/vector-icons';
 import {Colors} from "../../constants/colors";
 
-interface Props{
-    icon:keyof typeof Ionicons.glyphMap;
-    label?: string;
-    color?:string;
+interface Props {
+    icon: keyof typeof Ionicons.glyphMap;
     size: number;
-    onPress?: ()=>void;
+    label?: string;
+    color?: string;
+    onPress?: () => void;
 }
 
-export const IconButton:FC<Props> = ({icon, color,label, size, onPress}) => (
+export const IconButton: FC<Props> = ({icon, color, label, size, onPress}) => (
     <View style={styles.button}><Pressable style={styles.button} onPress={onPress}><Text
         style={styles.text}>{label}</Text><Ionicons name={icon} color={color} size={size}/></Pressable></View>)
 
 const styles = StyleSheet.create({
-    button:{
-      justifyContent:"center",
-      alignItems:"center",
-        flexDirection:"row",
+    button: {
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
         marginRight: 5
 
     },
